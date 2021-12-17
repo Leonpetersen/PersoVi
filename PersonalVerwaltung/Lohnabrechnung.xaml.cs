fa02af63eddd890f9cef3808e8b97a36c4f65cc2
Leon Petersen <lepe8506@stud.hs-flensburg.de>
2021-12-17 01:21:52 +0100

    Payroll features

--- PersonalVerwaltung/Lohnabrechnung.xaml.cs
+++ PersonalVerwaltung/Lohnabrechnung.xaml.cs
@@ -23,6 +23,74 @@ namespace PersonalVerwaltung
         public Lohnabrechnung()
         {
             InitializeComponent();
+
+            //ComboBox Monate füllen
+            combo_month.Items.Add("Januar");
+            combo_month.Items.Add("Februar");
+            combo_month.Items.Add("März");
+            combo_month.Items.Add("April");
+            combo_month.Items.Add("Mai");
+            combo_month.Items.Add("Juni");
+            combo_month.Items.Add("Juli");
+            combo_month.Items.Add("August");
+            combo_month.Items.Add("September");
+            combo_month.Items.Add("Oktober");
+            combo_month.Items.Add("November");
+            combo_month.Items.Add("Dezember");
+
+            //ComboBox Jahre füllen
+            int aktuellesJahr = DateTime.Now.Year;
+            List<int> jahreList = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                jahreList.Add(aktuellesJahr--);
+            }
+            combo_year.ItemsSource = jahreList;
+
+            //ComboBoxen initial belegen
+            combo_year.SelectedIndex = 0;
+
+            int aktuellerMonat = DateTime.Now.Month;
+            switch (aktuellerMonat)
+            {
+                case 1:
+                    combo_month.SelectedIndex = 0;
+                    break;
+                case 2:
+                    combo_month.SelectedIndex = 1;
+                    break;
+                case 3:
+                    combo_month.SelectedIndex = 2;
+                    break;
+                case 4:
+                    combo_month.SelectedIndex = 3;
+                    break;
+                case 5:
+                    combo_month.SelectedIndex = 4;
+                    break;
+                case 6:
+                    combo_month.SelectedIndex = 5;
+                    break;
+                case 7:
+                    combo_month.SelectedIndex = 6;
+                    break;
+                case 8:
+                    combo_month.SelectedIndex = 7;
+                    break;
+                case 9:
+                    combo_month.SelectedIndex = 8;
+                    break;
+                case 10:
+                    combo_month.SelectedIndex = 9;
+                    break;
+                case 11:
+                    combo_month.SelectedIndex = 10;
+                    break;
+                case 12:
+                    combo_month.SelectedIndex = 11;
+                    break;
+            }
+
         }
         private void Btn_Show(object sender, RoutedEventArgs e)
         {
@@ -41,6 +109,11 @@ namespace PersonalVerwaltung
             {
                 mainPanelBorder.Margin = new Thickness();
             }
+        }
+
+        private void showPayroll(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
---
+++ PersonalVerwaltung/Monat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalVerwaltung
+{
+    public class Monat
+    {
+        public int Monatsnummer { get; set; }
+        public string Bezeichnung { get; set; }
+    }
+}
